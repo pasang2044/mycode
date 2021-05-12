ba18deede63e0dd163c648f5d9fe8eed01d9c1cc
Pasang Sherpa <pasang.sherpa2044@gmail.com>
2021-05-12 17:04:04 +0000

    logic in python

---
+++ introfor/forloop1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# create the ist called vendors
+
+vendors =["cisco", "juniper", "big_ip", "f5", "arista"]
+
+# loop across the list vendors
+
+for x in vendors:
+    print ("The vendor is:" +x) # each time through the loop print value of x
+print("\n Our loops has ended.") # when the loop ends print this
