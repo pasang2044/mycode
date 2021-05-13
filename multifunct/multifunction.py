9ccbdf05095e5e92cd485b98f55ffda7d3e8f376
Pasang Sherpa <pasang.sherpa2044@gmail.com>
2021-05-13 22:05:32 +0000

    Function to multiply all the numbers in a list

---
+++ multifunct/multifunction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+total = 1
+number_list =[]
+
+n = int(input("Enter the desired list size "))
+print("\n")
+for i in range(0,n):
+    print("Enter the number at index", i)
+    item = int(input())
+    number_list.append(item)
+print("Here is your list ", number_list)
+for elements in range (0, len(number_list)):
+    total = total * number_list[elements]
+    
+print("The multiplied total of the list: ", total)
+
+
